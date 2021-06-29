2e0dd3ff2e1044d93613123a233bcc53fe46600f
francogallegosqu <francogallegos@gmail.com>
2021-06-29 15:51:40 -0500

    actualizando 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenInterceptorService } from '../usuarios/token-interceptor.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(public authService: TokenInterceptorService, private router: Router) { }
+  images:any=[]
+  constructor(public authService: TokenInterceptorService, private router: Router
+    ) {
+    this.images = [
+      {title: 'First Slide', short: 'First Slide Short', src: "https://picsum.photos/id/700/900/500"},
+      {title: 'Second Slide', short: 'Second Slide Short', src: "https://picsum.photos/id/1011/900/500"},
+      {title: 'Third Slide', short: 'Third Slide Short', src: "https://picsum.photos/id/984/900/500"}
+    ];
+    
+   }
 
   ngOnInit(): void {
+    
   }
 
   logout(): void {
